65043fd5c9c9673f5eae674f7dd61c2551557cf2
nicolas-m-rey <nic.m.rey@gmail.com>
2023-02-28 13:11:58 -0800

    Added avatar column to users.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_24_201120) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_28_211031) do
+  create_table "photos", force: :cascade do |t|
+    t.text "image_data"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -20,9 +24,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_24_201120) do
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
