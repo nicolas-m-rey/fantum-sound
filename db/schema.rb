27782152dced7abac14bc200bef63cfe6bd6e9e1
nicolas-m-rey <nic.m.rey@gmail.com>
2023-03-06 10:18:00 -0800

    Migrated comments to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_03_195113) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_06_181653) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "body"
+    t.integer "author_id"
+    t.integer "track_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "likes", force: :cascade do |t|
     t.integer "liker_id"
