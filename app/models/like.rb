280767b199f8266acecdb48cb9d5974a3323988c
nicolas-m-rey <nic.m.rey@gmail.com>
2023-03-06 10:09:38 -0800

    Added and configured Like model.

---
+++ app/models/like.rb
@@ -0,0 +1,12 @@
+class Like < ApplicationRecord
+    validates :liker_id, :track_id
+    validates_uniqueness_of :liker_id, scope: :track_id
+
+    belongs_to :liker,
+    foreign_key: :liker_id
+    class_name: :User
+
+    belongs_to :track,
+    foreign_key: :track_id,
+    class_name: :Track
+end
