f55b7dc1d3f6b7ae9155329fbf96028ba06f51dd
nicolas-m-rey <nic.m.rey@gmail.com>
2023-03-08 10:18:12 -0800

    Migrated added columns in account to database.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_08_181056) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_08_181543) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_181056) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
   end
 
   create_table "active_storage_attachments", force: :cascade do |t|
